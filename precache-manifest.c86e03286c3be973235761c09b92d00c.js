e6ba5c2b9a663470be335ec3064a1e437c19fe22
jayaharish <61152197+jayaharish@users.noreply.github.com>
2020-07-18 12:42:53 +0530

    Updates

---
+++ precache-manifest.c86e03286c3be973235761c09b92d00c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d625004e15e2d9904257cb4edb964de",
+    "url": "/coda-frontend/index.html"
+  },
+  {
+    "revision": "fcc76f846d1a52433f84",
+    "url": "/coda-frontend/static/css/main.22733d34.chunk.css"
+  },
+  {
+    "revision": "d1abefb1563bffcf52bb",
+    "url": "/coda-frontend/static/js/2.5630b1d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coda-frontend/static/js/2.5630b1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc76f846d1a52433f84",
+    "url": "/coda-frontend/static/js/main.bf716414.chunk.js"
+  },
+  {
+    "revision": "36e95a0e79cacb212ff4",
+    "url": "/coda-frontend/static/js/runtime-main.feab294b.js"
+  },
+  {
+    "revision": "bb5557317677de08603590958e9ae595",
+    "url": "/coda-frontend/static/media/product.bb555731.png"
+  }
+]);
